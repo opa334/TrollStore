48d22f738df0bd946083104c2b9fea3130864066
Nebula <github@itsnebula.net>
2022-10-06 20:53:08 -0400

    Check for developer IPA and auto make out folder

--- _compile/build_trollinstaller2.sh
+++ _compile/build_trollinstaller2.sh
@@ -1,12 +1,18 @@
+if [ ! -f "$(pwd)/target/Developer.ipa" ]; then
+    echo "[!] Developer IPA doesn't exist! Please place it in _compile/target/Developer.ipa"
+    exit
+fi
+
 cd ../TrollInstaller2
 make clean
 make package
 cd -
 
+mkdir -p ./out/devpwn
+
 lipo -thin arm64e ../TrollInstaller2/.theos/obj/debug/TrollInstaller2.app/TrollInstaller2 -output ./out/pwn_arm64e
 ldid -S -M -Kcert.p12 ./out/pwn_arm64e
 
-mkdir ./out/devpwn
 unzip target/Developer.ipa -d ./out/devpwn
 
 ./pwnify_compiled ./out/devpwn/Payload/Developer.app/Developer ./out/pwn_arm64e
